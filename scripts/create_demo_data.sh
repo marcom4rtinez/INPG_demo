b3e03812d624d216dd4b24196d3c8a54b5781d26
Marco Martinez <mmmartinez@bluewin.ch>
2024-09-23 19:48:39 +0000

    bump to 0.16

--- scripts/create_demo_data.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-poetry run infrahubctl run generators/create_basic.py
-poetry run infrahubctl run generators/create_location.py
-poetry run infrahubctl run generators/create_topology.py
-poetry run infrahubctl run generators/create_security_nodes.py
